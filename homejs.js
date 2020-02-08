95d450f6a4f6db5ced7b10813418922b37fdb813
ono-sys <g24.ono@gmail.com>
2020-02-08 17:42:54 +0900

    トップページ編しました

---
+++ homejs.js
@@ -0,0 +1,11 @@
+'use strict';
+
+$(function() {
+ $('.a').on('click', function() {
+    alert('ウイルスに感染しています')
+ });
+
+ $('.b').on('click', function() {
+    alert('ウイルスに感染しています')
+ });
+});
---
+++ index.js
@@ -0,0 +1,10 @@
+'use strict'
+
+$(function() {
+    $('.img-1').on('click', function(){
+        alert("え、山好きなんすか？")
+    });
+    $('.img-2').on('click', function(){
+        alert("え、酒好きなんすか？")
+    });
+});
